d71d43817952088ef93b07b81bba43023a6b6814
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-27 23:26:54 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,6 +18,7 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
   # Build an array that contains both of the above arrays
+  new_array = ORGANIC_PRODUCE.cancat(CONVENTIONAL_PRODUCE)
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
